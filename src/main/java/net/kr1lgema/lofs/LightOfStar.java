3d8a0822ae793679348a773a14d6c1a8d2865cec
Kr1l <gemayt90@gmail.com>
2025-05-18 15:49:05 +0300

    Initial commit

---
+++ src/main/java/net/kr1lgema/lofs/LightOfStar.java
@@ -0,0 +1,18 @@
+package net.kr1lgema.lofs;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LightOfStar implements ModInitializer {
+
+	public static final String MOD_ID = "lofs";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/kr1lgema/lofs/LightOfStarClient.java
@@ -0,0 +1,11 @@
+package net.kr1lgema.lofs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LightOfStarClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
